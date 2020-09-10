efe96f53e491c0cc3e728a6c3500645f92907b65
Carlos Auner Medina <auner.montejano@gmail.com>
2020-09-10 13:48:28 -0500

    Fixin secret key

--- Arduino_Morse_Code/settings.py
+++ Arduino_Morse_Code/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6izx2&u_-&s#x85!(u%=akk_3_+c^j#avic4l0rbc(k%ssmh*='
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
